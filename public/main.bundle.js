1c52ac16bf40f4b651201e2061031153f1e2df40
Chris Lucas <christopher.lucas11@gmail.com>
2017-07-10 23:07:58 +1000

    Gaming the life

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -74,7 +74,7 @@ var repeat = function repeat(fn, n) {
   return Array(n).fill(0).map(fn);
 };
 var rand = function rand() {
-  return Math.floor(Math.random() * 2);
+  return Math.random() < 0.75 ? 0 : 1;
 };
 var createGrid = function createGrid(n) {
   return repeat(function () {
@@ -92,17 +92,15 @@ var cellHeight = 40;
 var width = cellWidth * gridSize;
 var height = cellHeight * gridSize;
 
+var loops = 0;
+
 var canvas = document.getElementById('canvas');
 canvas.width = width;
 canvas.height = height;
 
 var context = canvas.getContext("2d");
 
-function drawBoard() {
-  context.strokeStyle = "black";
-  context.fillStyle = "black";
-
-  // Loop Through Grid
+function drawRect() {
   grid.forEach(function (yCell, y) {
     context.moveTo(0, y * cellHeight);
     context.lineTo(width, y * cellHeight);
@@ -114,6 +112,58 @@ function drawBoard() {
       }
     });
   });
+}
+function checkCells() {
+  grid.forEach(function (yCell, y) {
+    yCell.forEach(function (xCell, x) {
+      checkNeighbours(x, y);
+    });
+  });
+}
+
+function checkNeighbours(x, y) {
+  var left = y > 0 ? grid[y - 1][x] : 0;
+  var up = x > 0 ? grid[y][x - 1] : 0;
+  var down = x < grid[y].length - 1 ? grid[y][x + 1] : 0;
+  var right = y < grid.length - 1 ? grid[y + 1][x] : 0;
+  var upleft = y > 0 && x > 0 ? grid[y - 1][x - 1] : 0;
+  var upright = y > 0 && x < grid[y].length - 1 ? grid[y - 1][x + 1] : 0;
+  var downleft = y < grid.length - 1 && x > 0 ? grid[y + 1][x - 1] : 0;
+  var downright = y < grid.length - 1 && x < grid[y].length - 1 ? grid[y + 1][x + 1] : 0;
+  var sum = left + up + down + right + upleft + upright + downleft + downright;
+  if (sum < 2 && grid[y][x]) {
+    grid[y][x] = 0;
+  } else if (sum >= 2 && sum <= 3 && grid[y][x]) {
+    grid[y][x] = 1;
+  } else if (sum > 3 && grid[y][x]) {
+    grid[y][x] = 0;
+  } else if (!grid[y][x] && sum == 3) {
+    grid[y][x] = 1;
+  }
+}
+function updateGrid() {
+  checkCells();
+}
+function addRandom() {
+  var x = Math.floor(Math.random() * gridSize + 1);
+  var y = Math.floor(Math.random() * gridSize + 1);
+  console.log(x, y);
+  grid[y][x] = 1;
+}
+
+// Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+// Any live cell with two or three live neighbours lives on to the next generation.
+// Any live cell with more than three live neighbours dies, as if by overpopulation.
+// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+function drawBoard() {
+
+  context.clearRect(0, 0, width, height);
+  context.strokeStyle = "black";
+  context.fillStyle = "black";
+
+  // Loop Through Grid
+  drawRect();
 
   // Draw Bottom Line
   context.moveTo(0, height);
@@ -127,7 +177,21 @@ function drawBoard() {
   context.stroke();
 }
 
-drawBoard();
+function loop() {
+  loops++;
+  console.log(loops);
+  if (loops < 30) {
+    setTimeout(function () {
+      window.requestAnimationFrame(loop);
+    }, 500);
+  }
+
+  updateGrid();
+  drawBoard();
+  //addRandom();
+}
+
+window.addEventListener("load", loop);
 
 /***/ })
 /******/ ]);
--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,10 @@
 const repeat = (fn, n) => Array(n).fill(0).map(fn);
-const rand = () => Math.floor((Math.random() * 2));
+const rand = () => Math.random() < 0.75 ? 0 : 1; 
 const createGrid = n => repeat(() => repeat(rand, n), n);
 
 const gridSize = 10;
 
-const grid = createGrid(gridSize);
+let grid = createGrid(gridSize);
 
 const cellWidth = 40;
 const cellHeight = 40;
@@ -12,17 +12,15 @@ const cellHeight = 40;
 const width = cellWidth*gridSize;
 const height = cellHeight*gridSize;
 
+let loops = 0;
+
 let canvas = document.getElementById('canvas');
 canvas.width = width;
 canvas.height = height;
 
 var context = canvas.getContext("2d");
 
-function drawBoard(){
-    context.strokeStyle = "black";
-    context.fillStyle = "black";
-
-    // Loop Through Grid
+function drawRect(){
     grid.forEach((yCell, y) => {
       context.moveTo(0, y*cellHeight);
       context.lineTo(width, y*cellHeight);
@@ -34,6 +32,58 @@ function drawBoard(){
         }
       });
     });
+}
+function checkCells(){
+    grid.forEach((yCell, y) => {
+      yCell.forEach((xCell, x) => {
+        checkNeighbours(x, y)
+      });
+    });
+}
+
+function checkNeighbours(x, y) {
+  let left = (y > 0) ? grid[y-1][x] : 0;
+  let up = (x > 0) ? grid[y][x-1] : 0;
+  let down = (x < grid[y].length-1) ? grid[y][x+1] : 0;
+  let right = (y < grid.length-1) ? grid[y+1][x] : 0;
+  let upleft = (y > 0 && x > 0) ? grid[y-1][x-1] : 0;
+  let upright = (y > 0 && x < grid[y].length-1) ? grid[y-1][x+1] : 0;
+  let downleft = (y < grid.length-1 && x > 0) ? grid[y+1][x-1] : 0;
+  let downright = (y < grid.length-1 && x < grid[y].length-1) ? grid[y+1][x+1] : 0;
+  let sum = left+up+down+right+upleft+upright+downleft+downright; 
+  if(sum < 2 && grid[y][x]){
+    grid[y][x] = 0;
+  } else if(sum >= 2 && sum <= 3 && grid[y][x]){
+    grid[y][x] = 1; 
+  } else if(sum > 3 && grid[y][x]) {
+    grid[y][x] = 0; 
+  } else if(!grid[y][x] && sum == 3) {
+    grid[y][x] = 1;
+  }
+}
+function updateGrid() {
+   checkCells(); 
+}
+function addRandom() {
+  let x = Math.floor((Math.random() * gridSize) + 1);
+  let y = Math.floor((Math.random() * gridSize) + 1);
+  console.log(x, y);
+  grid[y][x] = 1;
+}
+
+// Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+// Any live cell with two or three live neighbours lives on to the next generation.
+// Any live cell with more than three live neighbours dies, as if by overpopulation.
+// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+function drawBoard(){
+
+    context.clearRect(0, 0, width, height);
+    context.strokeStyle = "black";
+    context.fillStyle = "black";
+
+    // Loop Through Grid
+    drawRect()
 
     // Draw Bottom Line
     context.moveTo(0, height);
@@ -47,4 +97,18 @@ function drawBoard(){
     context.stroke();
 }
 
-drawBoard();
+function loop () {
+  loops++;
+  console.log(loops);
+  if(loops < 30){
+    setTimeout(function(){
+      window.requestAnimationFrame(loop);
+    }, 500);
+  }
+  
+  updateGrid();
+  drawBoard();
+  //addRandom();
+}
+
+window.addEventListener("load", loop);
