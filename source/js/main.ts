4928af104dfa1be924f7dae04db50ae880cedcb8
Chris Lucas <christopher.lucas11@gmail.com>
2017-07-13 22:25:53 +1000

    Moving config into object, adding colours and stop function

--- source/js/main.ts
+++ source/js/main.ts
@@ -1,5 +1,20 @@
 import Grid from "./Grid";
 
-let GameOfLifeGrid = new Grid("canvas",100,10,10);
-GameOfLifeGrid.drawBoard();
+let GameOfLifeGrid = new Grid({
+  id: "canvas",
+  gridSize: 140,
+  cellWidth: 10,
+  cellHeight: 10,
+  lightness: 50
+});
 GameOfLifeGrid.run(true);
+let stopButton = document.querySelector('.stopBtn');
+let on = true;
+stopButton.addEventListener("click",function(){
+  if(on)
+    GameOfLifeGrid.stop();
+  else
+    GameOfLifeGrid.run(true);
+
+  on = !on;
+})
